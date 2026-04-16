269a2a31616937abae5256aa00244f48cbca2546
mnppi223 <mnppi223@gmail.com>
2026-04-16 12:56:31 +0900

    [Refactor] Section 파일 업로드 로직 제거 및 Lecture 도메인 분리 리팩토링
    자세한 내용은 PR 이슈에 작성하겠습니다.

--- src/main/java/com/wanted/legendkim/domain/course/CourseController.java
+++ src/main/java/com/wanted/legendkim/domain/course/CourseController.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 // Http 요청을 JSON 형태로 응답하는 컨트롤러를 Spring 에 전달
 @RestController
 // courseService 를 주입해주기 위해 사용
@@ -23,18 +25,26 @@ public class CourseController {
         <String> 은 응답을 문자열 메세지에 담는다는 뜻이다.
      */
     public ResponseEntity<String> courseInput(
-            // @PathVariable을 사용한 이유는 URL 경로를 받아오기 위해서 사용했다.
-            // 하지만 여기서는 받아올 URL 이 없기 때문에 @RequestParam 만 있으면 된다.
-            // 이를 통해서 API 명세서 기준의 값이 Body 로 전달되기 때문에 사용한다.
             @RequestParam("title") String title,
             @RequestParam("description") String description) {
 
-        // Controller 는 요청을 받는 역할을 한다. 실제 코스 등록
-        // 비즈니스 로직은 Service 에서 진행한다.
         courseService.registerCourse(title, description);
-        // 성공시 200이 나오게 한다.
         return ResponseEntity.ok("✅코스 등록를 성공했습니다!✅");
     }
 
+    /* comment. 흐름도
+        1. GET 요청을 받는다.
+        2. 서비스에서 데이터를 가져온다.
+        3. 성공 응답을 반환한다.
+     */
 
+    // 코스 등록을 위해서 작성한 로직
+    @GetMapping
+    // ResponseEintity 는 HTTP 응답 전체를 통제.
+    // 우리는 컨트롤러 안에서 비즈니스 로직을 처리하지 않으며, ControllerService 에서
+    // 비즈니스 로직을 처리한다.
+    public ResponseEntity<List<Course>> getCourseList() {
+        List<Course> courses = courseService.getCourseList();
+        return ResponseEntity.ok(courses);
+    }
 }
